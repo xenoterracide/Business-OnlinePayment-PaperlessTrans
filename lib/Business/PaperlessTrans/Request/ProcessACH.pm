ffc17eaf431c764757515a9fdcb1fdaf8f675fc1
Caleb Cushing <xenoterracide@gmail.com>
2013-05-18 03:08:09 -0500

    add process ACH request
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

---
+++ lib/Business/PaperlessTrans/Request/ProcessACH.pm
@@ -0,0 +1,38 @@
+package Business::PaperlessTrans::Request::ProcessACH;
+use strict;
+use warnings;
+use namespace::autoclean;
+
+# VERSION
+
+use Moose;
+use MooseX::Types::Common::String qw( NumericCode );
+
+extends 'Business::PaperlessTrans::Request';
+
+with qw(
+	Business::PaperlessTrans::Request::Role::Token
+	Business::PaperlessTrans::Request::Role::Test
+	Business::PaperlessTrans::Request::Role::Money
+	Business::PaperlessTrans::Request::Role::Profile
+);
+
+sub _build_type {
+	return 'ProcessACH';
+}
+
+has check => (
+	remote_name => 'Check',
+	isa         => 'Business::PaperlessTrans::RequestPart::Check',
+	is          => 'ro',
+);
+
+has check_number => (
+	remote_name => 'CheckNumber',
+	isa         => NumericCode,
+	is          => 'ro',
+);
+	
+__PACKAGE__->meta->make_immutable;
+1;
+# ABSTRACT: AuthorizeCard Request
