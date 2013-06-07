1d5cced920bb474c2e079d04e1d7a9a311289a72
Caleb Cushing <xenoterracide@gmail.com>
2013-06-07 08:38:59 -0500

    Build results of 9dd7a8b (on master)

--- lib/Business/OnlinePayment/PaperlessTrans.pm
+++ lib/Business/OnlinePayment/PaperlessTrans.pm
@@ -3,7 +3,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '0.001004'; # VERSION
+our $VERSION = '0.001005'; # VERSION
 
 use parent 'Business::OnlinePayment';
 
@@ -262,7 +262,7 @@ Business::OnlinePayment::PaperlessTrans - Interface to Paperless Transaction Cor
 
 =head1 VERSION
 
-version 0.001004
+version 0.001005
 
 =head1 SYNOPSIS
 
